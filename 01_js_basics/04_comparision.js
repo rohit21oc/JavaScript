76354de3d8fc0d1194c523612986a5802bafd888
ROHIT KUMAR <119073894+rohit21oc@users.noreply.github.com>
2023-12-30 15:23:05 +0530

    Add files via upload
    comparion

---
+++ 01_js_basics/04_comparision.js
@@ -0,0 +1,32 @@
+console.log(2<1);
+console.log(2>1);
+console.log(2<=1);
+console.log(2>=1);
+console.log(2==1);
+console.log(2!=1);
+
+
+// compare with null
+console.log("null>0: ", null>0);
+console.log("null<0: ", null<0);
+console.log("null==0: ",null==0);
+console.log("null>=0: ",null>=0);
+console.log("null<=0: ",null<=0);
+// conversion With undefined
+console.log("undefined==0: ",undefined==0);
+console.log("undefined!=0: ",undefined!=0);
+console.log("undefined>0: ",undefined>0);
+console.log("undefined<0: ",undefined<0);
+console.log("undefined>=0: ",undefined<0);
+console.log("undefined<=0: ",undefined<0);
+
+
+// we have to avoid from this type of conversion, because it can be confuse😂
+
+// Strictly conversion
+console.log("Strictly conversion");
+console.log("2" ==2);
+console.log("2" ===2);
+
+
+
