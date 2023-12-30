d29c2c23e2f0675ccaa62063d9bbb270d4d3b0ce
ROHIT KUMAR <119073894+rohit21oc@users.noreply.github.com>
2023-12-30 07:02:05 +0000

    datatypes

---
+++ 01_js_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+let name ="Rohit";
+let age= 20;
+let isLogedIn = null;
+let state;
+console.table([name,age,isLogedIn,state]);
+//Null => null is a special value that represents an empty or unknown value. For example, let number = null;
+
+// Number => numbers are primitive data types. For example, const a = 3; const b = 3.13; 1,2,3
+
+
+// Strings =>A string is a sequence of one or more characters that may consist of letters, numbers, or symbols => "name"
+
+
+// Symbol => Symbol is a built-in object whose constructor returns a symbol primitive — also called a Symbol value or just a Symbol — that's guaranteed to be unique. 
+
+
+// Undifined =>A variable that has not been assigned a value is of type undefined.
+
+
+// Boolean => A boolean value is one that can either be TRUE or FALSE.
+
+
+// BigInt => BigInt variables are used to store big integer values that are too big to be represented by a normal JavaScript Number 
