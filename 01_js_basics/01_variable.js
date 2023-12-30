54df0cf830f6b3299f69c0e426d69178393766db
ROHIT KUMAR <119073894+rohit21oc@users.noreply.github.com>
2023-12-30 06:32:05 +0000

    variables

---
+++ 01_js_basics/01_variable.js
@@ -0,0 +1,8 @@
+const accountId = 10010020;
+let email = "rohit@gmail.com";
+var password = 12345;
+accountCity = "Chennai";
+let accountState;
+console.log(accountId);
+
+console.table([accountId, email, password,accountId,accountState]);
