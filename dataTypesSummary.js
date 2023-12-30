3a204f85c9c20ef93d7691158660c9056dd02cd8
ROHIT KUMAR <119073894+rohit21oc@users.noreply.github.com>
2023-12-31 00:05:53 +0530

    Add files via upload

---
+++ dataTypesSummary.js
@@ -0,0 +1,19 @@
+// primitive Data types
+// 1.Number, 2. String, 3.Boolean, 4.Symbol, 5.Null, 6.Undefined, 7.BigInt
+
+// Non-Primitive Data Types
+// array ,object, function
+
+
+// Stack(Premitive) and Heap(Non-primitive);
+let userOne = {
+    email : "rohit@gmail.com",
+    pass:1234,
+    userId: "rohit21oc",
+};
+let userTwo = userOne;
+// console.log(userOne.email);
+console.log(userOne);
+console.log(userTwo);
+console.log(userOne.email);
+console.log(userTwo.email);
