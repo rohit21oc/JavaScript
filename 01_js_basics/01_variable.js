81606099f24fd995e70c81ba84419a4a0e8063a9
ROHIT KUMAR <119073894+rohit21oc@users.noreply.github.com>
2023-12-30 12:04:50 +0530

    Update 01_variable.js

--- 01_js_basics/01_variable.js
+++ 01_js_basics/01_variable.js
@@ -4,5 +4,9 @@ var password = 12345;
 accountCity = "Chennai";
 let accountState;
 console.log(accountId);
+/*
+Not prefer to use var
+because of issue of block scope and functional scope
+*/
 
 console.table([accountId, email, password,accountId,accountState]);
