a1e441a87745c8470d5933e450015f2a53b83cc2
ROHIT KUMAR <119073894+rohit21oc@users.noreply.github.com>
2023-12-30 15:06:52 +0530

    Update 03_conversionOperation.js
    operations

--- 01_js_basics/03_conversionOperation.js
+++ 01_js_basics/03_conversionOperation.js
@@ -19,3 +19,48 @@ console.log(typeof str);
 // Boolean
 // true =>0
 // false =>1
+
+
+/******************************** *Operation* ************************/
+
+let value = 3;
+let newValue= -value;
+console.log(newValue);
+console.log(typeof newValue);
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/2);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+(2+2));
+console.log(1+2+"2");
+console.log((3+4)*5%3);
+// true
+console.log(true);
+console.log(+true);
+console.log(-true);
+// console.log(true-);
+console.log(+"");
+
+// three number
+let num1,num2,num3;
+num1 = num2 = num3 = 2+2;
+console.log("num1: ",num1);
+console.log("num2: ",num2);
+console.log("num3: ",num3);
+
+
+
+let gameCounter = 100;
+console.log(gameCounter);
+gameCounter++;
+console.log(gameCounter++);
+console.log(gameCounter);
+++gameCounter;
+console.log(gameCounter);
+
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
