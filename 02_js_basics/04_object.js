8611ab96d9ad1e9c5d6b2e19acae79613b116c87
ROHIT KUMAR <119073894+rohit21oc@users.noreply.github.com>
2024-01-10 19:56:35 +0530

    Add files via upload

---
+++ 02_js_basics/04_object.js
@@ -0,0 +1,55 @@
+var obj = new Object(123);
+var newObj = {};
+newObj.id = 123;
+newObj.userName = "rohit21oc";
+newObj.email = "rohit@gmail.com";
+newObj.city = "Patna"
+// console.log(newObj);
+// console.log(obj);
+
+console.log(newObj);
+console.log(Object.keys(newObj));
+console.log(Object.values(newObj));
+console.log(Object.entries(newObj));
+console.log(newObj.hasOwnProperty("email"));
+
+
+var regularUser = {
+    email:"rohitkumar@gmail.com",
+    fullName:{
+        newFullName:{
+            firstName:"Rohit",
+            lastName:"Kumar"
+        }
+    }
+}
+console.log(regularUser.fullName.newFullName.firstName)
+var obj1 = { "a": 1, "b":2}
+var obj2 = { "c": 1, "d":2}
+var obj3 = { "e": 1, "f":2}
+var obj4 = { "g": 1, "h":2}
+
+// let obj3 = {obj1, obj2};
+// console.log(obj3);
+
+// var obj5 = Object.assign({}, obj1 ,obj2,obj3,obj4);
+// console.log(obj5);
+
+var obj5 = {...obj1,...obj2};
+console.log(obj5);
+
+const arr = [
+    {
+        id:5043,
+        userName:"rohit210c"
+    },
+    {
+        id:5043,
+        userName:"rohit210c"
+    },
+    {
+        id:5043,
+        userName:"rohit210c"
+    }
+]
+console.log(arr);
