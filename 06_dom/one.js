3ea2736610aa66cb6b71976bfc5fb1117d11ac8f
ROHIT KUMAR <119073894+rohit21oc@users.noreply.github.com>
2024-04-19 01:43:32 +0530

    Add files via upload

---
+++ 06_dom/one.js
@@ -0,0 +1,16 @@
+document.getElementById('first').setAttribute("class", "para1 firstPara")
+const cl = document.getElementById('first').className
+console.log(cl);
+title.style.backgroundColor = 'orange';
+title.style.textAlign= "center";
+title.style.borderRadius= '10px';
+title.style.border= '2px solid black';
+title.style.padding = '15px';
+
+
+console.log(title.innerText);
+console.log(title.textContent);
+
+console.log(title.innerHTML);
+// const myul = document.querySelector("ul");
+// myul.querySelector("li");
