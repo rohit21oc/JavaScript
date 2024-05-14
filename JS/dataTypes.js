c6866a61b1ef93bb815673f64386890387db9526
ROHIT KUMAR <119073894+rohit21oc@users.noreply.github.com>
2024-05-14 12:04:44 +0530

    Add files via upload
    JS By Vishwa Mohan

---
+++ JS/dataTypes.js
@@ -0,0 +1,31 @@
+var name = 'Rohit';
+console.log(name);
+console.log(typeof (name));
+
+
+var num = 94
+var num2 = 1.5
+console.log(typeof num2)
+
+let big_num= 12345679255466665464131n;
+console.log(typeof big_num);
+
+console.log(typeof NaN)
+
+console.log(typeof null)
+
+var name2;
+console.log(name2)
+
+console.log(typeof true);
+
+
+var s1 = Symbol();
+
+var person = {
+    name: 'rohit',
+    age:20,
+    num:1234567890,
+}
+console.log(person);
+console.log(typeof person);
---
+++ JS/operator.js
@@ -0,0 +1,61 @@
+// Assignment Operator
+var a = 10;
+var b = 20;
+console.log('value of a:',a)
+console.log('value of b:',b)
+console.log('a+b : ',a+b);
+console.log('a*b:',a*b);
+console.log('a**b:',a**b);
+console.log('b%a:',b%a);
+
+// increament
+var i = 1;
+i +=1
+console.log(i)
+console.log(i)
+i = i + 2
+console.log(i)
+// Post Increament
+// var j = 4;
+// console.log(j++)
+// console.log(j)
+
+// Pre Increament
+var j = 4;
+console.log(++j)
+console.log(j)
+
+
+// Relational Operator
+// Equality
+console.log(1==2);
+console.log(1=='1');
+
+// Strict Equality
+console.log(1==='1');
+
+// Check Inequality
+console.log(1 != '1')
+console.log(1 !== '1')
+
+// Comperative
+console.log(3>4)
+console.log(30>4)
+
+
+
+// Logical Operator
+console.log('Logical Operator')
+// OR
+console.log(3<4 || false)
+// And
+console.log(3<4 && false)
+// XOR
+console.log(true ^ false)
+
+// BitWise Operertor
+console.log(10 & 6)
+
+console.log(10 | 6)
+
+console.log(10 ^ 6)
